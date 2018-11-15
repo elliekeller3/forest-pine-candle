9de801840a7163d62a2ec597906f9b3210100e99
Landon Keller <landon@7oaksgroup.com>
2018-11-14 20:04:57 -0700

    poop

---
+++ client/src/components/Table.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <table>
+      <thead>
+        <tr>
+          <th>
+            Header 1
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>
+            Body 1
+          </td>
+          <td>
+            Body 2
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+  
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    headers: Array
+  }
+}
+</script>
+
+<style scoped>
+</style>
