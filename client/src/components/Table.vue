20fec4b0f85d8df8347f26aebd3f6e491c5dd008
elliekeller3 <elliekeller33@gmail.com>
2018-11-14 20:53:00 -0700

    poo

--- client/src/components/Table.vue
+++ client/src/components/Table.vue
@@ -1,36 +1,79 @@
+
 <template>
   <div>
-    <table>
+    <table class="table" v-on:click="penisPenis">
       <thead>
         <tr>
-          <th>
-            Header 1
+          <th v-for="element in headers"
+              :key="element"
+              class="border-bottom">
+            {{element}}
           </th>
         </tr>
       </thead>
       <tbody>
         <tr>
-          <td>
-            Body 1
+          <td class="border" v-for="cock in cells" :key="cock">
+           {{cock}}
           </td>
-          <td>
-            Body 2
+          
+        </tr>
+        <tr>
+          <td class="border" v-for="cock in cells" :key="cock">
+           {{cock}}
           </td>
+          
         </tr>
       </tbody>
     </table>
+    <span>{{confirmed? 'You confirmed' : 'You canceled'}}</span>
   </div>
   
 </template>
 
 <script>
+/* eslint-disable */
 export default {
-  name: 'HelloWorld',
+  name: 'table',
   props: {
-    headers: Array
+    headers: Array,
+    cells: Array
+  },
+  methods: {
+    penisPenis() {
+      if(confirm('hello it\'s me, penisPenis')) {
+        this.confirmed = true
+      } else {
+        this.confirmed = false
+      }
+
+    }
+  },
+  data() {
+    return {
+      confirmed: null
+    }
   }
 }
 </script>
 
 <style scoped>
+
+.table {
+  height: 200px;
+  width: 400px;
+  box-shadow: 0 0 2px 0 rgba(0,0,0,.14), 0 2px 2px 0 rgba(0,0,0,.12), 0 1px 3px 0 rgba(0,0,0,.2);
+  margin: 16px;
+  padding: 26px;
+  border-collapse: collapse
+}
+
+.border-bottom {
+  border-bottom: 1px solid gray;
+}
+
+.border {
+  border: 1px solid gainsboro; 
+}
+
 </style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,19 +1,19 @@
 <template>
   <div>
-    <Table v-bind:headers="['Penis', 'Pussy', 'Bum']"/>
-    <Table />
+    <Table v-bind:headers="['Penis', 'Pussy', 'Bum']" v-bind:cells="['computer', 'cock', 'balls']"/>
+    <Table v-bind:headers="['labia', 'light', 'boob']" v-bind:cells="['different', 'word', 'nipple']"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+/* eslint-disable */
 import HelloWorld from '@/components/HelloWorld.vue'
 import Table from '@/components/Table.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld,
     Table
   }
 }
